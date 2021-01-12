de36e9549f89c194116ca9be98907fb8125293a0
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-11 21:10:26 -0500

    Add Builder pattern

---
+++ Creational_Patterns/Builder/Builder/Builder/Program.cs
@@ -0,0 +1,12 @@
+﻿using Builder.Client;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new ClientCode().Main();
+        }
+    }
+}
