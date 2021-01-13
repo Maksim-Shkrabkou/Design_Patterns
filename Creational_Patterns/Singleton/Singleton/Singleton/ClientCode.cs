804cb354e9a975b91756e1e469edca0c854a6c26
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-13 15:35:42 -0500

    Add Singleton pattern

---
+++ Creational_Patterns/Singleton/Singleton/Singleton/ClientCode.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Threading;
+
+namespace Singleton
+{
+    public class ClientCode
+    {
+        public static void TestSingletonCreationCode()
+        {
+            Console.WriteLine(
+                "{0}\n{1}\n\n{2}\n",
+                "If you see the same value, then singleton was reused (yay!)",
+                "If you see different values, then 2 singletons were created (booo!!)",
+                "RESULT:"
+            );
+            
+            var process1 = new Thread(() =>
+            {
+                TestSingleton("FOO");
+            });
+            
+            var process2 = new Thread(() =>
+            {
+                TestSingleton("BAR");
+            });
+            
+            process1.Start();
+            process2.Start();
+            
+            process1.Join();
+            process2.Join();
+        }
+
+        private static void TestSingleton(string value)
+        {
+            var singleton = Singleton.GetInstance(value);
+            Console.WriteLine(singleton.Value);
+        } 
+    }
+}
