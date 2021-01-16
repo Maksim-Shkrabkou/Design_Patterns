16409436c63296444899352db8856829b0c12147
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-15 19:57:57 -0500

    Add Bridge pattern

---
+++ Structural_Patterns/Bridge/Bridge/Bridge/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Bridge.Abstration_Bridge;
+using Bridge.ClientCode;
+using Bridge.Implementation_Bridge;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client();
+            
+            Abstraction abstraction;
+            
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+            
+            Console.WriteLine();
+            
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
+        }
+    }
+    
+}
