3e6c6bee375c79b9b8345cb80dc12e7f4679a7de
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-18 20:46:17 -0500

    Add Facade pattern

---
+++ Structural_Patterns/Facade/Facade/Facade/Program.cs
@@ -0,0 +1,20 @@
+﻿using Facade.Client;
+using Facade.Facade;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code may have some of the subsystem's objects already
+            // created. In this case, it might be worthwhile to initialize the
+            // Facade with these objects instead of letting the Facade create
+            // new instances.
+            var subsystem1 = new Subsystem1();
+            var subsystem2 = new Subsystem2();
+            Facade.Facade facade = new Facade.Facade(subsystem1, subsystem2);
+            ClientCode.ClientCodeFacade(facade);
+        }
+    }
+}
