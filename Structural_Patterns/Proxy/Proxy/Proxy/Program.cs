f12f25d567edc7af82dc0bfda42b645f85dea480
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-20 13:45:08 -0500

    Add Proxy pattern

---
+++ Structural_Patterns/Proxy/Proxy/Proxy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Proxy.Client;
+using Proxy.ProxyPattern;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ClientCode();
+            
+            Console.WriteLine("Client: Executing the client code with a real subject:");
+            var realSubject = new RealSubject();
+            client.ClientProxyCode(realSubject);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Executing the same client code with a proxy:");
+            var proxy = new ProxyPattern.Proxy(realSubject);
+            client.ClientProxyCode(proxy);
+        }
+    }
+}
