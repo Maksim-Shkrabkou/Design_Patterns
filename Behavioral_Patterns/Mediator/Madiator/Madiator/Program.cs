fb5d2f45b7aa4544dc495cb2b0c1db6edde18fc4
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-24 15:01:34 -0500

    Add Mediator pattern

---
+++ Behavioral_Patterns/Mediator/Madiator/Madiator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Madiator.MediatorPattern;
+
+namespace Madiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var component1 = new Component1();
+            var component2 = new Component2();
+            new ConcreteMediator(component1, component2);
+
+            Console.WriteLine("Client triggets operation A.");
+            component1.DoA();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client triggers operation D.");
+            component2.DoD();
+        }
+    }
+}
