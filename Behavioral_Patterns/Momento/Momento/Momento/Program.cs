adecebacb96cd8de046d1da2be3ee6fb3042d9ed
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-25 20:47:02 -0500

    Add Momento pattern

---
+++ Behavioral_Patterns/Momento/Momento/Momento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Momento.MomentoPattern;
+
+namespace Momento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Client code.
+            var originator = new Originator("Super-duper-super-puper-super.");
+            var caretaker = new Caretaker(originator);
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            caretaker.Backup();
+            originator.DoSomething();
+
+            Console.WriteLine();
+            caretaker.ShowHistory();
+
+            Console.WriteLine("\nClient: Now, let's rollback!\n");
+            caretaker.Undo();
+
+            Console.WriteLine("\n\nClient: Once more!\n");
+            caretaker.Undo();
+
+            Console.WriteLine();
+        }
+    }
+}
