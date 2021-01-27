6cdfe3dfc0f52afed40307ba63272dd96d076a79
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-27 18:01:07 -0500

    Add State pattern

---
+++ Behavioral_Patterns/State/State/State/Program.cs
@@ -0,0 +1,15 @@
+﻿using State.StatePattern;
+
+namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
+}
---
+++ Behavioral_Patterns/State/State/State/StatePattern/ConcreteStateA.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace State.StatePattern
+{
+    // Concrete States implement various behaviors, associated with a state of
+    // the Context.
+    public class ConcreteStateA : State
+    {
+        public override void Handle1()
+        {
+            Console.WriteLine("ConcreteStateA handles request1.");
+            Console.WriteLine("ConcreteStateA wants to change the state of the context.");
+            this._context.TransitionTo(new ConcreteStateB());
+        }
+
+        public override void Handle2()
+        {
+            Console.WriteLine("ConcreteStateA handles request2.");
+        }
+    }
+}
