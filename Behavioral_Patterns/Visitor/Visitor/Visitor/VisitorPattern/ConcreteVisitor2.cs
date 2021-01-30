f53d030f4c2fede099ab746d1ee40b83785173bc
Maksim Shkrabkou <m.shkrabkou@gmail.com>
2021-01-30 14:34:11 -0500

    Add Visitor pattern

---
+++ Behavioral_Patterns/Visitor/Visitor/Visitor/VisitorPattern/ConcreteVisitor2.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Visitor.VisitorPattern
+{
+    public class ConcreteVisitor2 : IVisitor
+    {
+        public void VisitConcreteComponentA(ConcreteComponentA element)
+        {
+            Console.WriteLine(element.ExclusiveMethodOfConcreteComponentA() + " + ConcreteVisitor2");
+        }
+
+        public void VisitConcreteComponentB(ConcreteComponentB element)
+        {
+            Console.WriteLine(element.SpecialMethodOfConcreteComponentB() + " + ConcreteVisitor2");
+        }
+    }
+}
